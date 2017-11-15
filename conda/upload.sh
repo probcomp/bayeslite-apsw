d72858dbe94cf78553ae037cdec537072361a878
Aaron Vinson <avinson.public@gmail.com>
2017-11-14 23:40:21 -0800

    fix upload script

--- conda/upload.sh
+++ conda/upload.sh
@@ -6,13 +6,13 @@ if [ -n "${TRAVIS_TAG}" ]; then
   conda install anaconda-client
   # if tag didn't come from master, add the "dev" label
   if [ $(git branch --contains tags/${TRAVIS_TAG}) = "master" ]; then
-    anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} ~/miniconda/conda-bld/linux-64/crosscat-*.tar.bz2 --force
+    anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} ~/miniconda/conda-bld/linux-64/apsw-*.tar.bz2 --force
   else
-    anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l dev ~/miniconda/conda-bld/linux-64/crosscat-*.tar.bz2 --force
+    anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l dev ~/miniconda/conda-bld/linux-64/apsw-*.tar.bz2 --force
   fi
 elif [ ${TRAVIS_EVENT_TYPE} = "cron" ]; then
   conda install anaconda-client
-  anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l nightly ~/miniconda/conda-bld/linux-64/crosscat-*.tar.bz2 --force
+  anaconda -t ${CONDA_UPLOAD_TOKEN} upload -u ${CONDA_USER} -l nightly ~/miniconda/conda-bld/linux-64/apsw-*.tar.bz2 --force
 else
   exit 0
 fi
